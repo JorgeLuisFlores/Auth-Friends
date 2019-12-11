41ff4ab29771a334a88233589fe3715d0069d352
Jorge Flores <george130562@yahoo.com>
2019-12-10 20:35:56 -0800

    Finished

---
+++ friends/src/Components/Friends.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { axiosWithAuth } from "../Utils/AxiosWithAuth";
+
+const Friends = props => {
+    const { name, age, email, id } = props.data;
+
+    const [editing, setEditing] = useState(false);
+    const [friendValue, setFriendValue] = useState(props.data);
+
+    const handleDelete = () => {
+        props.setUpdate(true);
+
+
+        axiosWithAuth()
+            .delete("/friends/" + id)
+            .then(res => {
+
+                props.setList(res.data);
+                props.setUpdate(false);
+            })
+            .catch(err => {
+                console.log("Error: ", err);
+            });
+
+    };
+
+    const handleChange = e => {
+        setFriendValue({ ...friendValue, [e.target.name]: e.target.value });
+    };
+
+    const handleEdit = () => {
+        if (editing === true) {
+            axiosWithAuth()
+                .put(`/friends/${id}`, friendValue)
+                .then(res => {
+                    props.setList(res.data);
+                    setEditing(false);
+                })
+                .catch(err => {
+                    console.log("Error: ", err);
+                });
+        }
+    };
+    return (
+        <div className="friend">
+            <h2>
+                {editing ? (
+                    <input
+                        type="text"
+                        name="name"
+                        value={friendValue.name}
+                        onChange={handleChange}
+                    />
+                ) : (
+                        name
+                    )}
+            </h2>
+            <p>
+                Age:{" "}
+                {editing ? (
+                    <input
+                        type="text"
+                        name="age"
+                        value={friendValue.age}
+                        onChange={handleChange}
+                    />
+                ) : (
+                        age + "yrs old"
+                    )}
+            </p>
+            <p>
+                Email:{" "}
+                {editing ? (
+                    <input
+                        type="text"
+                        name="email"
+                        value={friendValue.email}
+                        onChange={handleChange}
+                    />
+                ) : (
+                        email
+                    )}
+            </p>
+            <div className="buttons">
+                <button onClick={handleDelete}>Delete Friend</button>
+                <button
+                    onClick={() => {
+                        handleEdit();
+                        setEditing(true);
+                    }}
+                >
+
+                    {editing ? "Submit Changes" : "Edit Friend"}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Friends; 
